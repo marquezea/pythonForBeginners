ab2288e20a313b0139e6944b39e7eb44c0bed17d
marquezea <51771581+marquezea@users.noreply.github.com>
2020-05-22 16:17:48 -0300

    Transformed with click

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -6,22 +6,20 @@ ec2 = session.resource('ec2')
 
 def filter_instances(project):
     instances = []
-
     if project:
         filters = [{'Name':'tag:Project', 'Values':[project]}]
         instances = ec2.instances.filter(Filters=filters)
     else:
         instances = ec2.instances.all()
-    
     return instances
 
 @click.group()
+@click.option('--profile', default=None, help="Use a given AWS profile")
 def instances():
-    """ Commands for instances"""
+    """Commands for instances"""
 
 @instances.command('list')
-@click.option('--project', default=None,
-    help="Only instances for project (tag Project=<name>)")
+@click.option('--project', default=None, help="Only instances for project (tag Project=<name>)")
 def list_instances(project):
     "List EC2 instances and its status"
     instances = filter_instances(project)
@@ -38,31 +36,24 @@ def list_instances(project):
     return 
 
 @instances.command('stop')
-@click.option('--project', default=None, 
-    help='Only instances for project')
+@click.option('--project', default=None, help='Only instances for project')
 def stop_instances(project):
     "Stop EC2 instances"
     instances = filter_instances(project)
-    
     for i in instances:
         print('Stopping {0}...'.format(i.id))
         i.stop()
-    
     return
 
 @instances.command('start')
-@click.option('--project', default=None, 
-    help='Only instances for project')
-def stop_instances(project):
+@click.option('--project', default=None, help='Only instances for project')
+def start_instances(project):
     "Start EC2 instances"
     instances = filter_instances(project)
-    
     for i in instances:
         print('Starting {0}...'.format(i.id))
         i.start()
-    
     return
-
 
 if __name__ == '__main__':
     instances()
